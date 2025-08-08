99ec7d4bab526797da8713c8c07b0191bb13c28f
Adarsh Pandey <adarshdon1920@gmail.com>
2025-08-08 08:43:18 +0530

    fix

--- home/settings.py
+++ home/settings.py
@@ -17,7 +17,9 @@ import os
 DEBUG = os.getenv("DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = [
-    'https://website-omh3.onrender.com'
+    'website-omh3.onrender.com',
+    'localhost',
+    '127.0.0.1'
 ]
 
 # Application definition
